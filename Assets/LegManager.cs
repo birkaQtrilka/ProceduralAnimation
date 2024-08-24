3b96c25c2fd8753e57cbc69012dac93ba50a10ad
Birka <birkavidit@gmail.com>
2024-08-24 15:40:17 +0200

    made a leg that completely follows the target

--- Assets/LegManager.cs
+++ Assets/LegManager.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class LegManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] BoxCollider _legPrefab;
+    [SerializeField, Range(1,20)] int _jointCount = 1;
+    [SerializeField] Transform _target;
+
+    Member[] _leg;
+
     void Start()
     {
-        
+        _leg = new Member[_jointCount];
+        for (int i = 0; i < _jointCount; i++)
+        {
+            _leg[i] = new Member(Instantiate(_legPrefab, transform));
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Member legBase = _leg[0];
+
+        legBase.SetBoxTransform(_target.position);
+        for (int i = 1; i < _leg.Length; i++)
+        {
+            Member current = _leg[i];
+            Member previous = _leg[i - 1];
+            current.SetBoxTransform(previous.GetEndPosition());
+        }
+    }
+
+    
+}
+
+public class Member
+{
+    public readonly BoxCollider Box;
+
+    public Member(BoxCollider box)
+    {
+        Box = box;
+    }
+
+    public void SetBoxTransform(Vector3 target)
+    {
+        Box.transform.up = (target - Box.transform.position).normalized;
+        float l = Box.transform.lossyScale.y;
+        Box.transform.position = target - l * .5f * Box.transform.up;
+    }
+
+    public Vector3 GetEndPosition()
+    {
+        float l = Box.transform.lossyScale.y;
+        return Box.transform.position - l * .5f * Box.transform.up;
     }
 }
